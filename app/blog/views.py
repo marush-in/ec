4bb938c2785563e317eba2067e183de9c26f716d
marush-in <shin.marumoto@gmail.com>
2019-04-11 17:43:56 +0900

    BlogAppのルーティングとビューの構築

--- app/blog/views.py
+++ app/blog/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from django.views.generic import ListView, DetailView
 
-# Create your views here.
+from .models import Post, Category
+
+
+class PostListView(ListView):
+    model = Post
+    queryset = Post.objects.filter(is_published=True).order_by('-created_at')
+    paginate_by = 12
+    context_object_name = 'posts'
+    template_name = 'blog/index.html'
+
+
+class PostDetailView(DetailView):
+    model = Post
+
+
+class PostListByCategoryView(ListView):
+    model = Post
+    paginate_by = 12
+    context_object_name = 'posts'
+    template_name = 'blog/posts-by-category.html'
+
+    def get_queryset(self):
+        category = get_object_or_404(Category, slug=self.kwargs['slug'])
+        queryset = Post.objects.filter(
+            category=category, is_published=True
+        ).order_by('-created_at')
+        return queryset
--- app/config/urls.py
+++ app/config/urls.py
@@ -5,7 +5,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
-    # path('', include('blog.urls')),
+    path('blog/', include('blog.urls')),
 ]
 
 if settings.DEBUG:
