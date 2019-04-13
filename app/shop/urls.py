ffc48240abbafe73a55f30ba3ea5aff162b69b4d
marush-in <shin.marumoto@gmail.com>
2019-04-13 12:11:39 +0900

    ECのサブページの追加

--- app/shop/urls.py
+++ app/shop/urls.py
@@ -6,5 +6,9 @@ app_name = 'shop'
 urlpatterns = [
     path('', views.index, name='index'),
     path('guide/', views.guide, name='guide'),
-    path('privacy_policy/', views.privacy_policy, name='privacy_policy')
+    path('privacy_policy/', views.privacy_policy, name='privacy_policy'),
+    path('company/', views.privacy_policy, name='company'),
+    path('terms_of_use/', views.terms_of_use, name='terms_of_use'),
+    path('faq/', views.faq, name='faq'),
+    path('legal/', views.legal, name='legal'),
 ]
--- app/shop/views.py
+++ app/shop/views.py
@@ -11,3 +11,19 @@ def guide(request):
 
 def privacy_policy(request):
     return render(request, 'shop/privacy_policy.html')
+
+
+def company(request):
+    return render(request, 'shop/company.html')
+
+
+def terms_of_use(request):
+    return render(request, 'shop/terms_of_use.html')
+
+
+def faq(request):
+    return render(request, 'shop/faq.html')
+
+
+def legal(request):
+    return render(request, 'shop/legal.html')
