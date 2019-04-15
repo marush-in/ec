0c7e32645b37d2d7c179d0770438de0f26d1adab
marush-in <shin.marumoto@gmail.com>
2019-04-16 08:13:09 +0900

    Products Appのadmin作成

--- app/products/admin.py
+++ app/products/admin.py
@@ -1,1 +1,26 @@
 from django.contrib import admin
+from .models import Category, Brand, Product
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'slug', 'created_at', 'updated_at')
+    list_editable = ['name', 'slug']
+
+
+class BrandAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'slug', 'created_at', 'updated_at')
+    list_editable = ['name', 'slug']
+
+
+class ProductAdmin(admin.ModelAdmin):
+    summernote_fields = ('description',)
+    list_display = (
+        'id', 'name', 'slug', 'category', 'brand', 'is_published',
+    )
+    list_editable = ['is_published']
+    list_per_page = 20
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Brand, BrandAdmin)
+admin.site.register(Product, ProductAdmin)
