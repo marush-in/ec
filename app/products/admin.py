dccf61ca5fc323a2b8fb693a7ef26e07395cae80
marush-in <shin.marumoto@gmail.com>
2019-04-16 08:28:00 +0900

    Productappのadminの修正

--- app/products/admin.py
+++ app/products/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
+from django.db import models
+from django.forms import TextInput
+from django_summernote.admin import SummernoteModelAdmin
 from .models import Category, Brand, Product
 
 
@@ -12,13 +15,17 @@ class BrandAdmin(admin.ModelAdmin):
     list_editable = ['name', 'slug']
 
 
-class ProductAdmin(admin.ModelAdmin):
+class ProductAdmin(SummernoteModelAdmin):
     summernote_fields = ('description',)
     list_display = (
         'id', 'name', 'slug', 'category', 'brand', 'is_published',
     )
+    list_display_links = ['name']
     list_editable = ['is_published']
     list_per_page = 20
+    formfield_overrides = {
+        models.CharField: {'widget': TextInput(attrs={'size': '100'})},
+    }
 
 
 admin.site.register(Category, CategoryAdmin)
