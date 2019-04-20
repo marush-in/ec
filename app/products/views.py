f4d4e74ab88e5bfd7d4945223dfdebc8ec6ecbe2
marush-in <shin.marumoto@gmail.com>
2019-04-20 09:07:06 +0900

    Product detailのテンプレートの表示

--- app/products/views.py
+++ app/products/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'products/index.html')
+
+
+def detail(request):
+    return render(request, 'products/product_detail.html')
