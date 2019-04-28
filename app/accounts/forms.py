280679d2c07347d3676324c89046e3d19de4ac1e
marush-in <shin.marumoto@gmail.com>
2019-04-29 08:30:47 +0900

    フォームの定義

---
+++ app/accounts/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+
+from allauth.account.forms import SignupForm
+
+
+class CustomSignupForm(SignupForm):
+    full_name = forms.CharField(max_length=50)
+    name_kana = forms.CharField(max_length=50)
+
+    def signup(self, request, user):
+        user.full_name = self.cleaned_data['full_name']
+        user.name_kana = self.cleaned_data['name_kana']
+        user.save()
+        return user
