b9632bc4acb3ff4f22fc0a67082fe4057f0981ad
marush-in <shin.marumoto@gmail.com>
2019-04-29 09:18:33 +0900

    formsの実装

--- app/accounts/forms.py
+++ app/accounts/forms.py
@@ -7,9 +7,9 @@ class CustomSignupForm(SignupForm):
     full_name = forms.CharField(max_length=50)
     name_kana = forms.CharField(max_length=50)
 
-    def signup(self, request, user):
+    def save(self, request):
+        user = super(CustomSignupForm, self).save(request)
         user.full_name = self.cleaned_data['full_name']
         user.name_kana = self.cleaned_data['name_kana']
-        print(user.full_name)
         user.save()
         return user
