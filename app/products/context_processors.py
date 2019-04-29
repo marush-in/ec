3f87c1cbe133743c1fe88be47b38c6a8e841f837
marush-in <shin.marumoto@gmail.com>
2019-04-30 08:34:23 +0900

    ブランド表示数の変更

--- app/products/context_processors.py
+++ app/products/context_processors.py
@@ -2,7 +2,7 @@ from .models import Brand
 
 
 def get_products_info(request):
-    brand_list = Brand.objects.all()
+    brand_list = Brand.objects.order_by('-created_at')[:10]
     return dict(
         brand_list=brand_list,
     )
