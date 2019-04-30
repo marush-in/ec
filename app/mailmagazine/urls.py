2ea695ffe2ffd191621bc36e177374f0ae2503c5
marush-in <shin.marumoto@gmail.com>
2019-04-30 09:17:14 +0900

    メルマガ一覧のテンプレートの作成

--- app/mailmagazine/urls.py
+++ app/mailmagazine/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import confirm
+from .views import index, confirm
 
 app_name = 'mailmagazine'
 
 urlpatterns = [
+    path('', index, name='index'),
     path('confirm/', confirm, name='confirm'),
 ]
--- app/mailmagazine/views.py
+++ app/mailmagazine/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 
 
+def index(request):
+    return render(request, 'mailmagazine/index.html')
+
+
 def confirm(request):
     return render(request, 'mailmagazine/confirm.html')
