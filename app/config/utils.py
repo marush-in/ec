9cb2921620e6386bcc826ef14d5845dcd4e029c8
marush-in <shin.marumoto@gmail.com>
2019-05-06 08:36:44 +0900

    変数名の変更

--- app/config/utils.py
+++ app/config/utils.py
@@ -3,7 +3,7 @@ from fnmatch import fnmatch
 
 class global_ip_list(list):
     def __contains__(self, value):
-        for pattern in self:
-            if fnmatch(value, pattern):
+        for global_ip in self:
+            if fnmatch(value, global_ip):
                 return True
         return False
