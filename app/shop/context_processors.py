9b3740371087b8fefa513a19c64bbb21fb260f6f
marush-in <shin.marumoto@gmail.com>
2019-05-07 17:44:13 +0900

    いいね数を表示させた

--- app/shop/context_processors.py
+++ app/shop/context_processors.py
@@ -1,11 +1,17 @@
 from .models import Settings
 from blog.models import Category
+from products.models import Like
 
 
 def get_settgins(request):
     settings = Settings.objects.first()
     categories = Category.objects.order_by('-created_at')[:5]
+    number_likes = 0
+    if request.user.is_authenticated:
+        user = request.user
+        number_likes = Like.objects.filter(user=user).count()
     return dict(
         settings=settings,
         categories=categories,
+        number_likes=number_likes,
     )
