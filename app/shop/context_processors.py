db96fd434d42f5ce8f5f3fd44fb95021d3af396e
marush-in <shin.marumoto@gmail.com>
2019-05-08 16:34:54 +0900

    いいねのハートマークの実装をした

--- app/shop/context_processors.py
+++ app/shop/context_processors.py
@@ -7,10 +7,16 @@ def get_settgins(request):
     settings = Settings.objects.first()
     categories = Category.objects.order_by('-created_at')[:5]
     number_likes = 0
+    liked_products = []
     if request.user.is_authenticated:
-        number_likes = Like.objects.filter(user=request.user).count()
+        user = request.user
+        number_likes = Like.objects.filter(user=user).count()
+        likes = Like.objects.filter(user=user)
+        if likes:
+            liked_products = [like.product_id for like in likes]
     return dict(
         settings=settings,
         categories=categories,
         number_likes=number_likes,
+        liked_products=liked_products,
     )
