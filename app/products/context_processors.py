a1f51fc5cfa020ab1f87efbd4feccfade57a3399
marush-in <shin.marumoto@gmail.com>
2019-05-09 17:06:09 +0900

    カテゴリーをサイドバーに表示させた

--- app/products/context_processors.py
+++ app/products/context_processors.py
@@ -1,8 +1,10 @@
-from .models import Brand
+from .models import Brand, Category
 
 
 def get_products_info(request):
     brand_list = Brand.objects.order_by('-created_at')[:10]
+    category_list = Category.objects.order_by('-created_at')[:10]
     return dict(
         brand_list=brand_list,
+        category_list=category_list,
     )
